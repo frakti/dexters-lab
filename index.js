78aaaee39e6e83d05395076d4a90348e2558e744
Tomasz Sikora <frakti7@gmail.com>
2016-05-29 16:34:26 +0200

    create lodash wrapper with each method interception

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 const _ = require('lodash')
 
+const lodashFunctions = _.functions(_)
+
+const newLo = _.runInContext().mixin(_(lodashFunctions)
+  .keyBy()
+  .mapValues(function (funcName) {
+
+    return function (...args) {
+      console.info(`Invoked ${funcName} with args ${JSON.stringify(args)}`)
+      return _[funcName](...args)
+    }
+
+  })
+  .value()
+)
+
 const [,, scriptPath, testDataPath] = process.argv
 
 // dangerous for acceptable for first iteration
 const func = require(scriptPath)
 const data = require(testDataPath)
 
-console.info(func(_, data))
+console.info(func(newLo, data))
