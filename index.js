8dd8ed3a8ba83b02e1b513fbd634792f707b8177
Tomasz Sikora <frakti7@gmail.com>
2016-05-30 17:51:46 +0200

    nicer output

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const _ = require('lodash')
 
 const lodashFunctions = _.functions(_)
 
+let step = 0
 const newLo = _.runInContext().mixin(_(lodashFunctions)
   .keyBy()
   .mapValues(function (funcName) {
 
     return function (...args) {
-      console.info(`Invoked ${funcName} with args ${JSON.stringify(args)}`)
+      console.info(`Step ${++step}. Invoked '${funcName}' with args ${JSON.stringify(args)}`)
       const result = _[funcName](...args)
 
-      console.info(`With result: ${JSON.stringify(result)}`)
+      console.info('With result:', JSON.stringify(result, null, 2))
+      console.info(Array(50).join('-'))
       return result
     }
 
