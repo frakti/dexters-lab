27c7ed106402c268c3bed8ca1a9f34770752aae4
Tomasz Sikora <frakti7@gmail.com>
2016-06-07 00:49:53 +0200

    create LodashWrapper class

---
+++ client.js
@@ -0,0 +1,31 @@
+'use strict'
+
+class LodashWrapper {
+  constructor (_) {
+    this.steps = []
+    this.lodash = _.runInContext().mixin(
+      _(_) // let the Inception begins :-)
+      .functions()
+      .keyBy()
+      .mapValues(function (funcName) {
+        return function (...args) {
+          const result = _[funcName](...args)
+
+          steps.push({
+            step,
+            funcName,
+            args: JSON.stringify(args),
+            result: JSON.stringify(result, null, 2)
+          })
+
+          return result
+        }
+      })
+      .value()
+    )
+  }
+
+  get stats () {
+    return this.steps
+  }
+}
