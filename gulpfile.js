6066e682aecd25e42a84e9b72d93795f0c64f2c8
Tomasz Sikora <frakti7@gmail.com>
2016-12-05 23:57:33 +0100

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const browserify = require('browserify')
+const gulp = require('gulp')
+const source = require('vinyl-source-stream')
+const uglify = require('gulp-uglify')
+const buffer = require('vinyl-buffer')
+const gutil = require('gulp-util')
+
+const errorHandler = function (err) {
+  gutil.log('Error', gutil.colors.red(err))
+  gutil.beep()
+}
+
+const bundler = (options) => browserify(
+  Object.assign({
+    entries: ['client/main.js'],
+    debug: false,
+    transform: ['babelify'],
+    extensions: ['.jsx']
+  }, options)
+)
+
+const bundle = (bundler, outputSource) => {
+  bundler
+    .bundle()
+    .on('error', errorHandler)
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./docs'))
+}
+
+gulp.task('build', function () {
+  bundler()
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./docs'))
+})
+
+gulp.task('watch', () => {
+  const b = bundler({
+    plugin: ['watchify'],
+    cache: {},
+    packageCache: {}
+  })
+
+  b.on('update', () => bundle(b))
+
+  b.on('log', (log) => gutil.log(`main.js: ${log}`))
+
+  bundle(b)
+})
