4bcbb42dcfdf07429483e347b71a893e34aee047
Tomasz Sikora <frakti7@gmail.com>
2017-02-08 21:37:55 +0100

    Add JavaScript editor

---
+++ client/JavaScriptEditor.jsx
@@ -0,0 +1,38 @@
+'use strict'
+
+import React, {Component, PropTypes} from 'react'
+import ace from 'brace'
+import 'brace/mode/html'
+import 'brace/theme/monokai'
+
+export default class JavaScriptEditor extends Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    defaultValue: PropTypes.string,
+    height: PropTypes.string
+  }
+
+  static defaultProps = {
+    readOnly: false,
+    height: '200px'
+  }
+
+  componentDidMount () {
+    this.editor = ace.edit(this.refs.editor)
+    const session = this.editor.getSession()
+    session.setMode('ace/mode/javascript')
+    session.setTabSize(2)
+    session.setUseWrapMode(true)
+    this.editor.setTheme('ace/theme/monokai')
+    this.editor.setValue(this.props.defaultValue || '')
+    this.editor.clearSelection()
+    this.editor.on('change', () => {
+      this.props.onChange(session.getValue())
+    })
+    this.editor.setShowPrintMargin(false)
+  }
+
+  render () {
+    return <div ref='editor' style={{width: '100%', height: this.props.height}}></div>
+  }
+}
--- client/MainPage.jsx
+++ client/MainPage.jsx
@@ -1,14 +1,30 @@
 'use strict'
 
-
+import _ from 'lodash'
 import React, {Component} from 'react'
+import JavaScriptEditor from './JavaScriptEditor'
 
 export default class Editor extends Component {
+  state = {
+    form: {
+      content: ''
+    }
+  }
+
+  onChangeContent = (content) => {
+    this.setState(({form}) => _.set(form, 'content', content))
+  }
+
   render () {
+    const {form: {content}} = this.state
     return (
       <div>
         <div className='editors'>
             Editor
+            <JavaScriptEditor
+              onChange={this.onChangeContent}
+              defaultValue={content}
+            />
         </div>
         <div className='preview'>
             Preview
