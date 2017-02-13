f59b5fdf829ca4df5f2f5954c96d578cfddc4e8c
Tomasz Sikora <frakti7@gmail.com>
2017-02-13 08:57:07 +0100

    Bugfix didn't show results for chained methods
    The case when last method returns 'static' result

--- client/LodashWrapper.js
+++ client/LodashWrapper.js
@@ -5,17 +5,40 @@ export default class LodashWrapper {
     this.steps = []
     const self = this
     let step = 1
-    this.lodash = _.runInContext().mixin(
-      _(_) // let the Inception begins :-)
-      .functions()
-      .keyBy()
-      .mapValues(function (funcName) {
-        return function (...args) {
-          const result = _[funcName](...args)
+    // this.lodash = _.runInContext().mixin(
+    //   _(_) // let the Inception begins :-)
+    //   .functions()
+    //   .keyBy()
+    //   .mapValues(function (funcName) {
+    //     return function (...args) {
+    //       const result = _[funcName](...args)
+    //
+    //       self.steps.push({
+    //         step: step++,
+    //         funcName,
+    //         args: JSON.stringify(args, null, 2),
+    //         result: JSON.stringify(result, null, 2)
+    //       })
+    //
+    //       return result
+    //     }
+    //   })
+    //   .value()
+    // )
+
+    this.lodash = _.runInContext()
+
+    // Proxy prototype methods
+    _(this.lodash.prototype)
+      .omit(['constructor', 'toJSON', 'value'])
+      .forEach((body, name) => {
+        const originalFunction = this.lodash.prototype[name]
+        this.lodash.prototype[name] = function (...args) {
+          const result = originalFunction.call(this, ...args)
 
           self.steps.push({
             step: step++,
-            funcName,
+            funcName: name,
             args: JSON.stringify(args, null, 2),
             result: JSON.stringify(result, null, 2)
           })
@@ -23,8 +46,6 @@ export default class LodashWrapper {
           return result
         }
       })
-      .value()
-    )
   }
 
   get stats () {
