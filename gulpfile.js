dcdb45fa1decbdba32d924fab60515c4c823bafa
Tomasz Sikora <frakti7@gmail.com>
2017-02-19 17:34:35 +0100

    Introduce sass in the project

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const uglify = require('gulp-uglify')
 const buffer = require('vinyl-buffer')
 const gutil = require('gulp-util')
 const serve = require('gulp-serve')
+const sass = require('gulp-sass')
 
 const errorHandler = function (err) {
   gutil.log('Error', gutil.colors.red(err))
@@ -21,6 +22,12 @@ const bundler = (options) => browserify(
     extensions: ['.jsx']
   }, options)
 )
+
+const buildStyles = (entry) => {
+  return gulp.src(entry)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./docs'))
+}
 
 const bundle = (bundler, outputSource) => {
   bundler
@@ -39,7 +46,15 @@ gulp.task('build', function () {
     .pipe(gulp.dest('./docs'))
 })
 
-gulp.task('watch', () => {
+gulp.task('styles', function () {
+  return buildStyles('./styles/main.scss')
+})
+
+gulp.task('styles:watch', ['styles'], function () {
+  return gulp.watch('./styles/*.scss', ['styles'])
+})
+
+gulp.task('watch', ['styles:watch'], () => {
   const b = bundler({
     plugin: ['watchify'],
     cache: {},
