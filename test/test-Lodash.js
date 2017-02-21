52ad8713647a1c007d5ba6392db9cde23c3acd42
Tomasz Sikora <frakti7@gmail.com>
2017-02-21 21:13:38 +0100

    Cover record steps of explicit method chain sequences invocation

--- test/test-Lodash.js
+++ test/test-Lodash.js
@@ -7,6 +7,38 @@ const LodashWrapper = require('../client/wrappers/Lodash')
 const TEST_DATA = require('./fixtures')
 
 describe('wrapper: Lodash ()', function () {
+  it('SHOULD record steps of explicit method chain sequences invocation', function () {
+    const wrapper = new LodashWrapper(_)
+    const data = [{city: 'Rybnik'}, {city: 'Katowice'}]
+
+    wrapper.lodash.chain(data).map('city').sortBy().first().value()
+
+    expect(wrapper.stats)
+      .to.be.eql([
+        {
+          step: 1,
+          funcName: 'map',
+          isChained: true,
+          result: ['Rybnik', 'Katowice'],
+          args: ['city']
+        },
+        {
+          step: 2,
+          funcName: 'sortBy',
+          isChained: true,
+          result: ['Katowice', 'Rybnik'],
+          args: []
+        },
+        {
+          step: 4,
+          funcName: 'first',
+          isChained: true,
+          result: 'Katowice',
+          args: []
+        }
+      ])
+  })
+
   it('SHOULD record steps of implicit method chain sequences invocation', function () {
     const wrapper = new LodashWrapper(_)
     const data = [{city: 'Rybnik'}, {city: 'Katowice'}]
