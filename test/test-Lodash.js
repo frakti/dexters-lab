778b45e4f076495759ec6852f3ff2d6e8f615d38
Tomasz Sikora <frakti7@gmail.com>
2017-02-21 20:45:05 +0100

    Cover single method invocation stats

--- test/test-Lodash.js
+++ test/test-Lodash.js
@@ -7,6 +7,24 @@ const LodashWrapper = require('../client/wrappers/Lodash')
 const TEST_DATA = require('./fixtures')
 
 describe('wrapper: Lodash ()', function () {
+  it('SHOULD return single step for _.map', function () {
+  it('SHOULD return single step stats for _.map', function () {
+  it('SHOULD record single step stats for _.map invocation', function () {
+    const wrapper = new LodashWrapper(_)
+    const data = [{city: 'Rybnik'}]
+
+    wrapper.lodash.map(data, 'city')
+
+    expect(wrapper.stats)
+      .to.be.eql([{
+        step: 1,
+        funcName: 'map',
+        isChained: false,
+        result: ['Rybnik'],
+        args: [[{city: 'Rybnik'}], 'city']
+      }])
+  })
+
   it('SHOULD return expected lodash transform', function () {
     const wrapper = new LodashWrapper(_)
 
