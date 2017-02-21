a98898923c8aa3d7b9f0de86e4696efd722d1376
Tomasz Sikora <frakti7@gmail.com>
2017-02-21 22:25:37 +0100

    Move proxied lodash to getter

--- client/wrappers/Lodash.js
+++ client/wrappers/Lodash.js
@@ -6,34 +6,40 @@ const cloneDeep = require('lodash/cloneDeep')
 module.exports = class LodashWrapper {
   constructor (_) {
     this.steps = []
+    this._ = _
+  }
 
-    const record = (name, isChained, args, result) => {
-      if (name === 'chain' || typeof (name) === 'undefined') {
-        return
-      }
-
-      const last = _.last(this.steps)
-      /**
-       * When we use chaining, some of short fusion functions fire `thru`
-       * making steps list confusing, We are going to remove that step.
-       * Those methods are not marked as `isChained` they are
-       * by default "not" chainable (you can't chain further as long as
-       * you didn't use _.chain())
-       */
-      if (
-        shortcutFusionFunctions.includes(name) &&
-        last && last.funcName === 'thru' && last.isChained
-      ) {
-        this.steps.pop()
-      }
-
-      this.steps.push({
-        funcName: name,
-        isChained,
-        args: cloneDeep(args),
-        result: cloneDeep(result)
-      })
+  record (name, isChained, args, result) {
+    const _ = this._
+    if (name === 'chain' || typeof (name) === 'undefined') {
+      return
     }
+
+    const last = _.last(this.steps)
+    /**
+    * When we use chaining, some of short fusion functions fire `thru`
+    * making steps list confusing, We are going to remove that step.
+    * Those methods are not marked as `isChained` they are
+    * by default "not" chainable (you can't chain further as long as
+    * you didn't use _.chain())
+    */
+    if (
+      shortcutFusionFunctions.includes(name) &&
+      last && last.funcName === 'thru' && last.isChained
+    ) {
+      this.steps.pop()
+    }
+
+    this.steps.push({
+      funcName: name,
+      isChained,
+      args: cloneDeep(args),
+      result: cloneDeep(result)
+    })
+  }
+
+  get lodash () {
+    const _ = this._
 
     const availableFunctions = _.keys(_)
 
@@ -43,27 +49,25 @@ module.exports = class LodashWrapper {
           return original[propertyName]
         }
 
-        // console.info(`Getting ${propertyName}`)
         original[propertyName].dexterLabFuncName = propertyName
         return new Proxy(original[propertyName], handler)
       },
 
       apply: (original, thisArg, args) => {
-        // console.info(`Calling ${original.dexterLabFuncName}`)
         const result = original.apply(thisArg, args)
 
         if (result && result.__wrapped__) {
-          record(original.dexterLabFuncName, true, args, result.value())
+          this.record(original.dexterLabFuncName, true, args, result.value())
           return new Proxy(result, handler)
         }
 
-        record(original.dexterLabFuncName, false, args, result)
+        this.record(original.dexterLabFuncName, false, args, result)
         return result
       }
     }
 
      // let the Inception begins :-)
-    this.lodash = new Proxy(_.runInContext(), handler)
+    return new Proxy(_.runInContext(), handler)
   }
 
   get stats () {
